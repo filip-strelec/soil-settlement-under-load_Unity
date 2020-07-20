aa3f59b84e5fef7bfe32252d32aa886893859615
filip4428 <filip.strelec@gmail.com>
2020-07-21 00:22:32 +0200

    Fixed bug for last dot

--- Assets/Scripts/SteinbrennerFormula.cs
+++ Assets/Scripts/SteinbrennerFormula.cs
@@ -29,40 +29,45 @@ List<double> valueList = new List<double> ();
 {
 
 
+
+
 double trenutnaDubinaZ = i;
+
+double razlikaDubine = programState.dubinaZ- trenutnaDubinaZ;
+
 
     double firstFraction = (double)Math.Atan((lengthL * widthB) / (trenutnaDubinaZ*Math.Sqrt(Math.Pow(lengthL,2) + Math.Pow(widthB,2)+ Math.Pow(trenutnaDubinaZ ,2))));
     double secondFraction = (double)(lengthL * widthB * trenutnaDubinaZ) / (Math.Sqrt(Math.Pow(lengthL , 2) + Math.Pow(widthB, 2) + Math.Pow(trenutnaDubinaZ, 2)));
     double thirdFraction = (double)(((1) / (Math.Pow(lengthL ,2) + Math.Pow(trenutnaDubinaZ,2))) + ((1) / (Math.Pow(widthB,2) + Math.Pow(trenutnaDubinaZ,2))));
     double steinbrennerIzracun = (double)((1) / (2 * Math.PI)) * (firstFraction + secondFraction * thirdFraction);
 
-    // Debug.Log("IDEEEMOOOOOOO___________IDEMOOO: "+trenutnaDubinaZ);
-    // Debug.Log("+++++++_______++++++++: "+steinbrennerIzracun);
+    Debug.Log("IDEEEMOOOOOOO___________IDEMOOO: "+trenutnaDubinaZ);
+    Debug.Log("+++++++_______++++++++: "+steinbrennerIzracun);
     depthList.Add(trenutnaDubinaZ);
     valueList.Add(steinbrennerIzracun);
 
-    double razlikaDubine = programState.dubinaZ- trenutnaDubinaZ;
+    
 
-//     if (razlikaDubine<= programState.inkrementMjerenjaZ){
+    if (razlikaDubine<= programState.inkrementMjerenjaZ){
 
-//         trenutnaDubinaZ = programState.dubinaZ;
+        trenutnaDubinaZ = programState.dubinaZ;
 
-//    firstFraction = (double)Math.Atan((programState.duzinaL * programState.sirinaB) / (trenutnaDubinaZ*Math.Sqrt(Math.Pow(programState.duzinaL,2) + Math.Pow(programState.sirinaB,2)+ Math.Pow(trenutnaDubinaZ,2))));
-//     secondFraction = (double)(programState.duzinaL * programState.sirinaB * trenutnaDubinaZ) / (Math.Sqrt(Math.Pow(programState.duzinaL, 2) + Math.Pow(programState.sirinaB, 2) + Math.Pow(trenutnaDubinaZ, 2)));
-//      thirdFraction = (double)(((1) / (Math.Pow(programState.duzinaL,2) + Math.Pow(trenutnaDubinaZ,2))) + ((1) / (Math.Pow(programState.sirinaB,2) + Math.Pow(trenutnaDubinaZ,2))));
-//     steinbrennerIzracun = (double)((1) / (2 * Math.PI)) * (firstFraction + secondFraction * thirdFraction);
+   firstFraction = (double)Math.Atan((lengthL * widthB) / (trenutnaDubinaZ*Math.Sqrt(Math.Pow(lengthL,2) + Math.Pow(widthB,2)+ Math.Pow(trenutnaDubinaZ,2))));
+    secondFraction = (double)(lengthL * widthB * trenutnaDubinaZ) / (Math.Sqrt(Math.Pow(lengthL, 2) + Math.Pow(widthB, 2) + Math.Pow(trenutnaDubinaZ, 2)));
+     thirdFraction = (double)(((1) / (Math.Pow(lengthL,2) + Math.Pow(trenutnaDubinaZ,2))) + ((1) / (Math.Pow(widthB,2) + Math.Pow(trenutnaDubinaZ,2))));
+    steinbrennerIzracun = (double)((1) / (2 * Math.PI)) * (firstFraction + secondFraction * thirdFraction);
 
-//     // Debug.Log("Zadnje: "+trenutnaDubinaZ);
-//     // Debug.Log("+++++++_______++++++++: "+steinbrennerIzracun);
+    Debug.Log("Zadnje: "+trenutnaDubinaZ);
+    Debug.Log("+++++++_______++++++++: "+steinbrennerIzracun);
 
-//      depthList.Add(trenutnaDubinaZ);
-//     valueList.Add(steinbrennerIzracun);
+     depthList.Add(trenutnaDubinaZ);
+    valueList.Add(steinbrennerIzracun);
 
-//     Debug.Log(trenutnaDubinaZ);
-//     Debug.Log(steinbrennerIzracun);
+    Debug.Log(trenutnaDubinaZ);
+    Debug.Log(steinbrennerIzracun);
 
 
-//     }
+    }
 
 }
 
